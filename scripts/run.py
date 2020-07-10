9d424a0e2beca6c94196d5ef105f821116382c83
Shervan Gharari <28273583+ShervanGharari@users.noreply.github.com>
2020-07-09 23:11:36 -0600

    Create run.py

---
+++ scripts/run.py
@@ -0,0 +1,21 @@
+#!/cvmfs/soft.computecanada.ca/easybuild/software/2017/Core/python/3.5.4/bin/python
+
+from shp_std.py import shp_std
+
+path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin/'
+field_name = 'segId'
+tolerance = 0.1 # 0.1 m2 in equal area projection
+stem = 'catEndoMERITpfaf_' # coomon part of the file names
+extension = '.gpkg'
+
+shp_std(stem+'71', extension, path, field_name, tolerance)
+shp_std(stem+'72', extension, path, field_name, tolerance)
+shp_std(stem+'73', extension, path, field_name, tolerance)
+shp_std(stem+'74', extension, path, field_name, tolerance)
+shp_std(stem+'75', extension, path, field_name, tolerance)
+shp_std(stem+'76', extension, path, field_name, tolerance)
+shp_std(stem+'77', extension, path, field_name, tolerance)
+shp_std(stem+'78', extension, path, field_name, tolerance)
+shp_std(stem+'81', extension, path, field_name, tolerance)
+shp_std(stem+'82', extension, path, field_name, tolerance)
+shp_std(stem+'83', extension, path, field_name, tolerance)
