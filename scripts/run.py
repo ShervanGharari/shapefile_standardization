44856d0a6e7afa9719cf8b6ee0f142ffffdc524e
Shervan Gharari <28273583+ShervanGharari@users.noreply.github.com>
2020-07-10 01:44:57 -0600

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,6 @@
 #!/cvmfs/soft.computecanada.ca/easybuild/software/2017/Core/python/3.5.4/bin/python
 
-from shp_std_def.py import *
+from shp_std_def import *
 
 path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin/'
 field_name = 'segId'
