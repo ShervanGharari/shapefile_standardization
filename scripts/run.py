950aa2696c0280876e6292a81cc6a3a1d02ed2c9
Shervan Gharari <28273583+ShervanGharari@users.noreply.github.com>
2020-07-10 14:45:23 -0600

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -2,21 +2,17 @@
 
 from shp_std_def import *
 
-path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin/'
+path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin_original/'
 field_name = 'segId'
 tolerance = 0.1 # 0.1 m2 in equal area projection
-stem = 'catEndoMERITpfaf_' # coomon part of the file names
-extension = '.gpkg'
+extension = '.shp'
 
 # North America Domain Shapes
-shp_std(stem+'71', extension, path, field_name, tolerance)
-shp_std(stem+'72', extension, path, field_name, tolerance)
-shp_std(stem+'73', extension, path, field_name, tolerance)
-shp_std(stem+'74', extension, path, field_name, tolerance)
-shp_std(stem+'75', extension, path, field_name, tolerance)
-shp_std(stem+'76', extension, path, field_name, tolerance)
-shp_std(stem+'77', extension, path, field_name, tolerance)
-shp_std(stem+'78', extension, path, field_name, tolerance)
-shp_std(stem+'81', extension, path, field_name, tolerance)
-shp_std(stem+'82', extension, path, field_name, tolerance)
-shp_std(stem+'83', extension, path, field_name, tolerance)
+shp_std(cat_pfaf_1_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_2_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_3_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_4_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_5_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_6_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_7_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
+shp_std(cat_pfaf_8_MERIT_Hydro_v07_Basins_v01, extension, path, field_name, tolerance)
