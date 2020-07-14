b9a02f0c48a79854bf1f23540fe3d3c4b267a0fa
ShervanGharari <shg096@gra-login3.graham.sharcnet>
2020-07-14 17:32:41 -0400

    change to bug fixed

--- scripts/run.py
+++ scripts/run.py
@@ -2,9 +2,10 @@
 
 from shp_std_def import *
 
-path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin_original/'
+# path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin_original/'
+path = '/project/6008034/Model_Output/ClimateForcingData/MERIT_Hydro_basin_bugfixed/'
 field_name = 'COMID'
-tolerance = 0.1 # 0.1 m2 in equal area projection
+tolerance = 0.0000001 # 0.1 m2 in equal area projection
 extension = '.shp'
 
 # North America Domain Shapes
@@ -14,5 +15,20 @@ extension = '.shp'
 #shp_std('cat_pfaf_4_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance)
 #shp_std('cat_pfaf_5_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance)
 #shp_std('cat_pfaf_6_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance)
-shp_std('cat_pfaf_7_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance) # North America
-shp_std('cat_pfaf_8_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance) # Arctic
+#shp_std('cat_pfaf_7_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance) # North America
+#shp_std('cat_pfaf_8_MERIT_Hydro_v07_Basins_v01', extension, path, field_name, tolerance) # Arctic
+
+#shp_std('cat_pfaf_71_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) # 
+shp_std('cat_pfaf_74_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_72_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_73_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_75_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_76_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_77_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_78_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_81_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_82_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_83_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_84_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_85_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
+shp_std('cat_pfaf_86_MERIT_Hydro_v07_Basins_v01_bugfix1', extension, path, field_name, tolerance) #
